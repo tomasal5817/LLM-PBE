cd18601ceff19c276c184fc5acb20d8cdf182fa2
tomasalander <tomasal@student.chalmers.se>
2025-02-18 13:31:19 +0100

    Uppdate AttackDemo.py

--- AttackDemo.py
+++ AttackDemo.py
@@ -1,11 +1,17 @@
+import os
 from data import JailbreakQueries
 from models import TogetherAIModels
 from attacks import Jailbreak
 from metrics import JailbreakRate
 
+api_key = os.getenv("TOGETHER_API_KEY")
+
+if not api_key:
+    raise ValueError("Not able to retrieve API Key from environment")
+
 data = JailbreakQueries()
-# Fill api_key
-llm = TogetherAIModels(model="togethercomputer/llama-2-7b-chat", api_key="")
+llm = TogetherAIModels(api_key=api_key, model="meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo", max_attempts=2)
+print("model:", llm.model)
 attack = Jailbreak()
 results = attack.execute_attack(data, llm)
 rate = JailbreakRate(results).compute_metric()

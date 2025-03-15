fda650ab6b10df1ac04fb8106f70176aa689a310
tomasalander <tomasal@student.chalmers.se>
2025-03-15 13:25:54 +0100

    try to run HF-model

--- attacks/PromptLeakage/prompt_leakage.py
+++ attacks/PromptLeakage/prompt_leakage.py
@@ -118,6 +118,16 @@ class PromptLeakage:
                     if tokenizer.chat_template is None:
                         raise NotImplementedError(f"No chat template for model: {model.arch}") 
                     inputs = tokenizer.apply_chat_template(msgs, tokenize=True, return_tensors="pt")
+                    
+                    # Debugging purpose
+                    print(f"Type of inputs: {type(inputs)}")  
+                    print(f"Keys in inputs: {inputs.keys()}") 
+                    if "input_ids" in inputs:
+                        print(f"Shape of input_ids: {inputs['input_ids'].shape}") 
+                        print(f"input_ids content: {inputs['input_ids']}")  
+                    else:
+                        raise ValueError("Key 'input_ids' not found in inputs dictionary!")
+                    
                     input_ids = inputs["input_ids"].unsqueeze(0)
                     with torch.no_grad():  
                         output = model._lm.generate(input_ids=input_ids, max_new_tokens=model.max_seq_len) 

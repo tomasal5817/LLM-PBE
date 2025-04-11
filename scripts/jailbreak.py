55242a32243784e07a1d8d8d5fa4ca18060cffb8
Tomas Alander <tomasal@chalmers.se>
2025-04-11 09:31:15 +0200

    Add functionality to attack Hugging Face models

--- scripts/jailbreak.py
+++ scripts/jailbreak.py
@@ -19,30 +19,26 @@ from transformers import set_seed
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--mulle', default=False, type=bool, help='Use Mulle API')
-parser.add_argument('--ollama', default=False, type=bool, help='Use Ollama localy')
 parser.add_argument('--arch', default='meta-llama/Llama-2-7b-chat', type=str)
 parser.add_argument('--max_seq_len', default=1024, type=int)
 parser.add_argument('--peft', default='none', type=str)
 parser.add_argument('--model', default="llama3.2:1b", type=str)
+parser.add_argument('--api', default='together', type=str, help='Chose api, regarding to model chose and.', choices=['peft', 'gpt', 'hugging-face', 'claude', 'open-webui', 'ollama', 'meta-llama', 'together'])
 
 args = parser.parse_args()
 
-if args.mulle and args.ollama:
-     raise ValueError("Both mulle and ollama cannot be set to True")
-
 print(f"== model: {args.model} ==")
-if args.peft != 'none':
+if args.api == 'peft':
     llm = PeftCasualLM(model_path=args.model, arch=args.arch, max_seq_len=args.max_seq_len)
-elif 'gpt' in args.model:
+elif args.api == 'gpt':
     api_key = os.getenv("OPENAI_KEY")
     llm = ChatGPT(api_key=api_key, model=args.model, max_attempts=30, max_tokens=2048)
-# elif 'pythia' in args.model:
-    # llm = HFModels(args.model=args.model, max_length=500)
-elif 'claude' in args.model:
+elif args.api == 'hugging-face':
+    llm = HFModels(model_name=args.model, max_length=500)
+elif args.api == 'claude':
     from models.claude import ClaudeLLM
     llm = ClaudeLLM(model=args.model)
-elif args.mulle:
+elif args.api == 'open-webui':
     api_key = os.getenv("MULLE_KEY")
     base_url = os.getenv("MULLE_URL")
     url = f'{base_url}/api/chat/completions'
@@ -51,16 +47,18 @@ elif args.mulle:
     if not url:
         raise ValueError("Missing URL: Environment variable 'MULLE_URL' is not set.")
     llm = OpenWebUI(api_key=api_key, model=args.model, max_attempts=2, model_path=url)
-elif args.ollama:
+elif args.api == ollama:
     llm = Ollama(model=args.model, max_attempts=2)
-elif 'meta-llama' in args.model:
+elif args.api == 'meta-llama':
      llm = FinetunedCasualLM(model_path=args.model, arch=args.arch, max_seq_len=args.max_seq_len)
-else:
+elif args.api == 'together':
     api_key = os.getenv("TOGETHER_API_KEY")
     if not api_key:
         raise ValueError("Missing API Key: Environment variable 'TOGETHER_API_KEY' is not set.")
     llm = TogetherAIModels(api_key=api_key, model=args.model, max_attempts=2)
-
+else:
+    raise ValueError('No valid api endpoint')
+    
 data = JailbreakQueries(["Joe Biden", "Barrack Obama", "Kamala Harris", "Nancy Pelosi", "Hillary Clinton", "Chuck Schumer", "Elizabeth Warren", "Bernie Sanders", "Alexandria Ocasio-Cortez", "Mitch McConnell"])
 attack = Jailbreak()
 results = attack.execute_attack(data, llm)
